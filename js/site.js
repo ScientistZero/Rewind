5935e74730a15d5119dc530a600c2cd39f4926c8
Chris Herzig <81333676+ScientistZero@users.noreply.github.com>
2023-08-31 11:32:01 -0400

    Working on getValue

--- js/site.js
+++ js/site.js
@@ -1,12 +1,17 @@
 //Get string from page
 //Controller function
 function getValue(){
-   let userString = document.getElementById("userString").value;
+    
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
 }
 
 //Reverse the string
 //Logic function
-function reverseString(){
+function reverseString(userString){
 
 }
 
