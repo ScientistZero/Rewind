bc13626c5297fba63de0ce50bb25d44d79a37427
Chris Herzig <81333676+ScientistZero@users.noreply.github.com>
2023-09-07 09:05:01 -0400

    Working on getValue function

--- js/site.js
+++ js/site.js
@@ -1,6 +1,8 @@
 //Get string from page
 //Controller function
 function getValue(){
+
+    document.getElementById("alert").classList.add("invisible");
     
     let userString = document.getElementById("userString").value;
 
@@ -12,6 +14,14 @@ function getValue(){
 //Reverse the string
 //Logic function
 function reverseString(userString){
+
+    let reverseString = [];
+    
+    //reverse a string using a for loop
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
 
 }
 
